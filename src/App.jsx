ada68a44751886ac818e756448f61b83d439170b
danielba777 <danielbauer230501@gmail.com>
2024-04-13 14:59:00 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+import SideBar from "./components/SideBar"
+
+function App() {
+
+  const [showModal, setShowModal] = useState(false)
+
+  function handleToggleModal(){
+    setShowModal(!showModal);
+  }
+
+  return (
+    <>
+      <Main />
+      {showModal && <SideBar handleToggleModal={handleToggleModal} />}
+      <Footer showModal={showModal} handleToggleModal={handleToggleModal} />
+    </>
+  )
+}
+
+export default App
