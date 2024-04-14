ba8c8478dea4811300a053b0b442a7d20cda5ac1
danielba777 <danielbauer230501@gmail.com>
2024-04-14 22:06:24 +0200

    API reference + loading screen
    Integrated the NASA API and fetch function for the picture and information. Created a loading screen that shows up when the data is being fetched.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 function App() {
 
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
   const [showModal, setShowModal] = useState(false)
 
   function handleToggleModal(){
     setShowModal(!showModal);
   }
 
+  useEffect(() =>{
+    async function fetchAPIData(){
+      
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+
+      try{
+
+        const res = await fetch(url)
+        const apiData = await res.json()
+        setData(apiData)
+        console.log('DATA\n', apiData)
+
+      }catch(err){
+        console.log(err.message)
+      }
+    }
+
+    fetchAPIData()
+  }, [])
+
   return (
     <>
-      <Main />
+      {data ? (<Main />) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
       {showModal && <SideBar handleToggleModal={handleToggleModal} />}
-      <Footer showModal={showModal} handleToggleModal={handleToggleModal} />
+      {data && (
+        <Footer showModal={showModal} handleToggleModal={handleToggleModal} />)}
     </>
   )
 }
